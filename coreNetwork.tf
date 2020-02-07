48f4798d850d42f3b3896a43bbb971ef18c5f3ac
Chris Walters <chris.walters@nhs.net>
2020-02-07 19:23:34 +0000

    end of lab3

---
+++ coreNetwork.tf
@@ -0,0 +1,69 @@
+resource "azurerm_resource_group" "core" {
+   name         = "core"
+   location     = var.loc
+   tags         = var.tags
+}
+
+resource "azurerm_public_ip" "vpnGatewayPublicIp" {
+  name                = "vpnGatewayPublicIp"
+  location            = var.loc
+  resource_group_name = azurerm_resource_group.core.name
+  allocation_method   = "Dynamic"
+
+  tags = var.tags
+}
+
+resource "azurerm_subnet" "GatewaySubnet" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = azurerm_resource_group.core.name
+  virtual_network_name = azurerm_virtual_network.core.name
+  address_prefix       = "10.0.0.0/24"
+}
+
+resource "azurerm_virtual_network" "core" {
+  name                = "core"
+  location            = var.loc
+  resource_group_name = azurerm_resource_group.core.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["1.1.1.1", "1.0.0.1"]
+
+  subnet {
+    name           = "GatewaySubnet"
+    address_prefix = "10.0.0.0/24"
+  }
+
+  subnet {
+    name           = "training"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "dev"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  tags = var.tags
+}
+
+/* resource "azurerm_virtual_network_gateway" "vpnGateway" {
+  name                = "vpnGateway"
+  location            = var.loc
+  resource_group_name = azurerm_resource_group.core.name
+
+  type     = "Vpn"
+  vpn_type = "RouteBased"
+
+  enable_bgp    = true
+  sku           = "Basic"
+
+  ip_configuration {
+    name                          = "vpnGwConfig1"
+    public_ip_address_id          = azurerm_public_ip.vpnGatewayPublicIp.id
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_subnet.GatewaySubnet.id
+  }
+
+  tags = var.tags
+
+}
+*/
